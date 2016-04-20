5c005a6a9d3fedd6a13034ee351ad583ec579aef
Nathan L <adamnate@gmail.com>
2016-04-19 18:22:51 -0700

    ready for heroku deploy

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/project-01");
+mongoose.connect(process.env.MONGOLAB_URI ||
+                      process.env.MONGOHQ_URL || "mongodb://localhost/project-01");
 
 module.exports.Location = require("./location.js");
 module.exports.Deal = require("./deal.js");
